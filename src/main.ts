bbc0729376cf604695079460b7df27e6706c5fa2
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-03-12 01:49:42 +0400

    docs: setup Open API docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { parse } from 'yaml';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const file = readFileSync(join(__dirname, '../doc/api.yaml'), 'utf8');
+  const document = parse(file);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
